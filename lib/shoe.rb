c9910f2766b63054498867c5139a53bd09722d23
Ryan McCahon <mccahonr@yahoo.com>
2019-08-20 14:41:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,7 @@ class Shoe
     @brand
   end
   def cobble
-    puts "Your shoe is good as new!"
+    puts "Your shoe is as good as new!"
     if @condition == "old"
       @condition = "new"
     end
